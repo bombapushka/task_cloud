6d19ba9b10984ff67746c46e7b38cdf155f52971
Vadim <wadkou@mail.ru>
2025-02-23 17:34:28 +0500

    Ш коммит

--- internal/server/middlewares/jwt_middleware.go
+++ internal/server/middlewares/jwt_middleware.go
@@ -17,6 +17,7 @@ func UserMiddleware(next http.Handler) http.Handler {
 		cookie, err := r.Cookie("token")
 		if err != nil {
 			http.Error(w, "Токен отсутствует", http.StatusUnauthorized)
+			http.Redirect(w, r, "/login", http.StatusFound)
 			return
 		}
 
